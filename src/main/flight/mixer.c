7f3c818e68e84fb940b051455f74b9c9ba05696b
Petri Mattila <petri.t.j.mattila@gmail.com>
2020-07-14 00:02:18 +0100

    HF3D: Remove THROTTLE_CORRECTION

--- src/main/flight/mixer.c
+++ src/main/flight/mixer.c
@@ -88,7 +88,6 @@ float motor_disarmed[MAX_SUPPORTED_MOTORS];
 mixerMode_e currentMixerMode;
 static motorMixer_t currentMixer[MAX_SUPPORTED_MOTORS];
 
-static FAST_RAM_ZERO_INIT int throttleAngleCorrection;
 
 static const motorMixer_t mixerQuadX[] = {
     { 1.0f, -1.0f,  1.0f, -1.0f },          // REAR_R
@@ -469,7 +468,7 @@ static void calculateThrottleAndCurrentMotorEndpoints(timeUs_t currentTimeUs)
     float currentThrottleInputRange = 0;
 
     {
-        throttle = rcCommand[THROTTLE] - PWM_RANGE_MIN + throttleAngleCorrection;
+        throttle = rcCommand[THROTTLE] - PWM_RANGE_MIN;
         float appliedMotorOutputLow = motorOutputLow;
 #if defined(USE_BATTERY_VOLTAGE_SAG_COMPENSATION)
         float motorRangeAttenuationFactor = 0;
@@ -666,11 +665,6 @@ FAST_CODE_NOINLINE void mixTable(timeUs_t currentTimeUs, uint8_t vbatPidCompensa
         // Apply the mix to motor endpoints
         applyMixToMotors(motorMix, activeMixer);
     }
-}
-
-void mixerSetThrottleAngleCorrection(int correctionValue)
-{
-    throttleAngleCorrection = correctionValue;
 }
 
 float mixerGetThrottle(void)
