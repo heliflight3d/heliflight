pmattila/scratchpad

HF3D: Improved debug_e

src/main/build/debug.c
@@ -31,69 +31,71 @@ uint8_t debugMode;
 uint32_t sectionTimes[2][4];
 #endif
 
-// Please ensure that these names are aligned with the enum values defined in 'debug.h'
+#define DEBUG_NAME(x)  [DEBUG_ ## x] = #x
+
+// Please ensure that names listed here match the enum values defined in 'debug.h'
 const char * const debugModeNames[DEBUG_COUNT] = {
-    "NONE",
-    "CYCLETIME",
-    "BATTERY",
-    "GYRO_FILTERED",
-    "ACCELEROMETER",
-    "PIDLOOP",
-    "GYRO_SCALED",
-    "RC_INTERPOLATION",
-    "ANGLERATE",
-    "ESC_SENSOR",
-    "SCHEDULER",
-    "STACK",
-    "ESC_SENSOR_RPM",
-    "ESC_SENSOR_TMP",
-    "ALTITUDE",
-    "FFT",
-    "FFT_TIME",
-    "FFT_FREQ",
-    "RX_FRSKY_SPI",
-    "RX_SFHSS_SPI",
-    "GYRO_RAW",
-    "DUAL_GYRO_RAW",
-    "DUAL_GYRO_DIFF",
-    "MAX7456_SIGNAL",
-    "MAX7456_SPICLOCK",
-    "SBUS",
-    "FPORT",
-    "RANGEFINDER",
-    "RANGEFINDER_QUALITY",
-    "LIDAR_TF",
-    "ADC_INTERNAL",
-    "RUNAWAY_TAKEOFF",
-    "SDIO",
-    "CURRENT_SENSOR",
-    "USB",
-    "UNUSED_0",
-    "RTH",
-    "ITERM_RELAX",
-    "ACRO_TRAINER",
-    "RC_SMOOTHING",
-    "RX_SIGNAL_LOSS",
-    "RC_SMOOTHING_RATE",
-    "UNUSED_1",
-    "DYN_LPF",
-    "RX_SPEKTRUM_SPI",
-    "DSHOT_RPM_TELEMETRY",
-    "RPM_FILTER",
-    "UNUSED_2",
-    "AC_CORRECTION",
-    "AC_ERROR",
-    "DUAL_GYRO_SCALED",
-    "DSHOT_RPM_ERRORS",
-    "CRSF_LINK_STATISTICS_UPLINK",
-    "CRSF_LINK_STATISTICS_PWR",
-    "CRSF_LINK_STATISTICS_DOWN",
-    "BARO",
-    "GPS_RESCUE_THROTTLE_PID",
-    "UNUSED_3",
-    "FF_LIMIT",
-    "FF_INTERPOLATED",
-    "BLACKBOX_OUTPUT",
-    "GYRO_SAMPLE",
-    "RX_TIMING",
+    DEBUG_NAME(NONE),
+    DEBUG_NAME(CYCLETIME),
+    DEBUG_NAME(BATTERY),
+    DEBUG_NAME(GYRO_FILTERED),
+    DEBUG_NAME(ACCELEROMETER),
+    DEBUG_NAME(PIDLOOP),
+    DEBUG_NAME(GYRO_SCALED),
+    DEBUG_NAME(RC_INTERPOLATION),
+    DEBUG_NAME(ANGLERATE),
+    DEBUG_NAME(ESC_SENSOR),
+    DEBUG_NAME(SCHEDULER),
+    DEBUG_NAME(STACK),
+    DEBUG_NAME(ESC_SENSOR_RPM),
+    DEBUG_NAME(ESC_SENSOR_TMP),
+    DEBUG_NAME(ALTITUDE),
+    DEBUG_NAME(FFT),
+    DEBUG_NAME(FFT_TIME),
+    DEBUG_NAME(FFT_FREQ),
+    DEBUG_NAME(RX_FRSKY_SPI),
+    DEBUG_NAME(RX_SFHSS_SPI),
+    DEBUG_NAME(GYRO_RAW),
+    DEBUG_NAME(DUAL_GYRO_RAW),
+    DEBUG_NAME(DUAL_GYRO_DIFF),
+    DEBUG_NAME(MAX7456_SIGNAL),
+    DEBUG_NAME(MAX7456_SPICLOCK),
+    DEBUG_NAME(SBUS),
+    DEBUG_NAME(FPORT),
+    DEBUG_NAME(RANGEFINDER),
+    DEBUG_NAME(RANGEFINDER_QUALITY),
+    DEBUG_NAME(LIDAR_TF),
+    DEBUG_NAME(ADC_INTERNAL),
+    DEBUG_NAME(RUNAWAY_TAKEOFF),
+    DEBUG_NAME(SDIO),
+    DEBUG_NAME(CURRENT_SENSOR),
+    DEBUG_NAME(USB),
+    DEBUG_NAME(UNUSED_0),
+    DEBUG_NAME(RTH),
+    DEBUG_NAME(ITERM_RELAX),
+    DEBUG_NAME(ACRO_TRAINER),
+    DEBUG_NAME(RC_SMOOTHING),
+    DEBUG_NAME(RX_SIGNAL_LOSS),
+    DEBUG_NAME(RC_SMOOTHING_RATE),
+    DEBUG_NAME(UNUSED_1),
+    DEBUG_NAME(DYN_LPF),
+    DEBUG_NAME(RX_SPEKTRUM_SPI),
+    DEBUG_NAME(DSHOT_RPM_TELEMETRY),
+    DEBUG_NAME(RPM_FILTER),
+    DEBUG_NAME(UNUSED_2),
+    DEBUG_NAME(AC_CORRECTION),
+    DEBUG_NAME(AC_ERROR),
+    DEBUG_NAME(DUAL_GYRO_SCALED),
+    DEBUG_NAME(DSHOT_RPM_ERRORS),
+    DEBUG_NAME(CRSF_LINK_STATISTICS_UPLINK),
+    DEBUG_NAME(CRSF_LINK_STATISTICS_PWR),
+    DEBUG_NAME(CRSF_LINK_STATISTICS_DOWN),
+    DEBUG_NAME(BARO),
+    DEBUG_NAME(GPS_RESCUE_THROTTLE_PID),
+    DEBUG_NAME(UNUSED_3),
+    DEBUG_NAME(FF_LIMIT),
+    DEBUG_NAME(FF_INTERPOLATED),
+    DEBUG_NAME(BLACKBOX_OUTPUT),
+    DEBUG_NAME(GYRO_SAMPLE),
+    DEBUG_NAME(RX_TIMING),
 };

heliflight3d/heliflight

src/main/build/debug.c
@@ -31,69 +31,71 @@ uint8_t debugMode;
 uint32_t sectionTimes[2][4];
 #endif
 
-// Please ensure that these names are aligned with the enum values defined in 'debug.h'
+#define DEBUG_NAME(x)  [DEBUG_ ## x] = #x
+
+// Please ensure that names listed here match the enum values defined in 'debug.h'
 const char * const debugModeNames[DEBUG_COUNT] = {
-    "NONE",
-    "CYCLETIME",
-    "BATTERY",
-    "GYRO_FILTERED",
-    "ACCELEROMETER",
-    "PIDLOOP",
-    "GYRO_SCALED",
-    "RC_INTERPOLATION",
-    "ANGLERATE",
-    "ESC_SENSOR",
-    "SCHEDULER",
-    "STACK",
-    "ESC_SENSOR_RPM",
-    "ESC_SENSOR_TMP",
-    "ALTITUDE",
-    "FFT",
-    "FFT_TIME",
-    "FFT_FREQ",
-    "RX_FRSKY_SPI",
-    "RX_SFHSS_SPI",
-    "GYRO_RAW",
-    "DUAL_GYRO_RAW",
-    "DUAL_GYRO_DIFF",
-    "MAX7456_SIGNAL",
-    "MAX7456_SPICLOCK",
-    "SBUS",
-    "FPORT",
-    "RANGEFINDER",
-    "RANGEFINDER_QUALITY",
-    "LIDAR_TF",
-    "ADC_INTERNAL",
-    "UNUSED_31",
-    "SDIO",
-    "CURRENT_SENSOR",
-    "USB",
-    "UNUSED_35",
-    "RTH",
-    "ITERM_RELAX",
-    "ACRO_TRAINER",
-    "RC_SMOOTHING",
-    "RX_SIGNAL_LOSS",
-    "RC_SMOOTHING_RATE",
-    "UNUSED_42",
-    "DYN_LPF",
-    "RX_SPEKTRUM_SPI",
-    "DSHOT_RPM_TELEMETRY",
-    "RPM_FILTER",
-    "UNUSED_47",
-    "AC_CORRECTION",
-    "AC_ERROR",
-    "DUAL_GYRO_SCALED",
-    "DSHOT_RPM_ERRORS",
-    "CRSF_LINK_STATISTICS_UPLINK",
-    "CRSF_LINK_STATISTICS_PWR",
-    "CRSF_LINK_STATISTICS_DOWN",
-    "BARO",
-    "GPS_RESCUE_THROTTLE_PID",
-    "UNUSED_57",
-    "FF_LIMIT",
-    "FF_INTERPOLATED",
-    "BLACKBOX_OUTPUT",
-    "GYRO_SAMPLE",
-    "RX_TIMING",
+    DEBUG_NAME(NONE),
+    DEBUG_NAME(CYCLETIME),
+    DEBUG_NAME(BATTERY),
+    DEBUG_NAME(GYRO_FILTERED),
+    DEBUG_NAME(ACCELEROMETER),
+    DEBUG_NAME(PIDLOOP),
+    DEBUG_NAME(GYRO_SCALED),
+    DEBUG_NAME(RC_INTERPOLATION),
+    DEBUG_NAME(ANGLERATE),
+    DEBUG_NAME(ESC_SENSOR),
+    DEBUG_NAME(SCHEDULER),
+    DEBUG_NAME(STACK),
+    DEBUG_NAME(ESC_SENSOR_RPM),
+    DEBUG_NAME(ESC_SENSOR_TMP),
+    DEBUG_NAME(ALTITUDE),
+    DEBUG_NAME(FFT),
+    DEBUG_NAME(FFT_TIME),
+    DEBUG_NAME(FFT_FREQ),
+    DEBUG_NAME(RX_FRSKY_SPI),
+    DEBUG_NAME(RX_SFHSS_SPI),
+    DEBUG_NAME(GYRO_RAW),
+    DEBUG_NAME(DUAL_GYRO_RAW),
+    DEBUG_NAME(DUAL_GYRO_DIFF),
+    DEBUG_NAME(MAX7456_SIGNAL),
+    DEBUG_NAME(MAX7456_SPICLOCK),
+    DEBUG_NAME(SBUS),
+    DEBUG_NAME(FPORT),
+    DEBUG_NAME(RANGEFINDER),
+    DEBUG_NAME(RANGEFINDER_QUALITY),
+    DEBUG_NAME(LIDAR_TF),
+    DEBUG_NAME(ADC_INTERNAL),
+    DEBUG_NAME(UNUSED_31),
+    DEBUG_NAME(SDIO),
+    DEBUG_NAME(CURRENT_SENSOR),
+    DEBUG_NAME(USB),
+    DEBUG_NAME(UNUSED_35),
+    DEBUG_NAME(RTH),
+    DEBUG_NAME(ITERM_RELAX),
+    DEBUG_NAME(ACRO_TRAINER),
+    DEBUG_NAME(RC_SMOOTHING),
+    DEBUG_NAME(RX_SIGNAL_LOSS),
+    DEBUG_NAME(RC_SMOOTHING_RATE),
+    DEBUG_NAME(UNUSED_42),
+    DEBUG_NAME(DYN_LPF),
+    DEBUG_NAME(RX_SPEKTRUM_SPI),
+    DEBUG_NAME(DSHOT_RPM_TELEMETRY),
+    DEBUG_NAME(RPM_FILTER),
+    DEBUG_NAME(UNUSED_47),
+    DEBUG_NAME(AC_CORRECTION),
+    DEBUG_NAME(AC_ERROR),
+    DEBUG_NAME(DUAL_GYRO_SCALED),
+    DEBUG_NAME(DSHOT_RPM_ERRORS),
+    DEBUG_NAME(CRSF_LINK_STATISTICS_UPLINK),
+    DEBUG_NAME(CRSF_LINK_STATISTICS_PWR),
+    DEBUG_NAME(CRSF_LINK_STATISTICS_DOWN),
+    DEBUG_NAME(BARO),
+    DEBUG_NAME(GPS_RESCUE_THROTTLE_PID),
+    DEBUG_NAME(UNUSED_57),
+    DEBUG_NAME(FF_LIMIT),
+    DEBUG_NAME(FF_INTERPOLATED),
+    DEBUG_NAME(BLACKBOX_OUTPUT),
+    DEBUG_NAME(GYRO_SAMPLE),
+    DEBUG_NAME(RX_TIMING),
 };