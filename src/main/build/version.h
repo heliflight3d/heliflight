0869bf44129366ea0b20941b33ee6db3d5f17a08
Petri Mattila <petri.t.j.mattila@gmail.com>
2020-09-02 16:36:51 +0100

    HF3D: Change firmware identifier to HLFL

--- src/main/build/version.h
+++ src/main/build/version.h
@@ -22,11 +22,9 @@
 
 #include "common/utils.h"
 
-// HF3D: These can be changed once Configurator is updated.
-//#define FC_FIRMWARE_NAME            "Heliflight"
-//#define FC_FIRMWARE_IDENTIFIER      "HLFL"
-#define FC_FIRMWARE_NAME            "Betaflight"
-#define FC_FIRMWARE_IDENTIFIER      "BTFL"
+#define FC_FIRMWARE_NAME            "Heliflight"
+#define FC_FIRMWARE_IDENTIFIER      "HLFL"
+
 #define FC_VERSION_MAJOR            4  // increment when a major release is made (big new feature, etc)
 #define FC_VERSION_MINOR            2  // increment when a minor release is made (small new feature, change etc)
 #define FC_VERSION_PATCH_LEVEL      2  // increment when a bug is fixed
