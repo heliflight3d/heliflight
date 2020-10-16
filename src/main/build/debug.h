df71ec426909b58f6a93fd1f8a00182c8f1240e2
Petri Mattila <petri.t.j.mattila@gmail.com>
2020-10-16 15:45:14 +0100

    HF3D: Fix DEBUG_SET macro
    DEBUG_SET() is a macro, that is used like a C-statement in the code.
    However, it is defined as a C-statement block. This is just
    asking for trouble. Define it properly as a statement.

--- src/main/build/debug.h
+++ src/main/build/debug.h
@@ -24,7 +24,7 @@
 extern int16_t debug[DEBUG16_VALUE_COUNT];
 extern uint8_t debugMode;
 
-#define DEBUG_SET(mode, index, value) {if (debugMode == (mode)) {debug[(index)] = (value);}}
+#define DEBUG_SET(mode, index, value)   do { if (debugMode == (mode)) debug[(index)] = (value); } while(0)
 
 #define DEBUG_SECTION_TIMES
 
